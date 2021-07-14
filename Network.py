741f9524361893cb4a85ace8701f549f8416ef53
Ubuntu <ubuntu@ubuntu2010.linuxvmimages.local>
2021-07-14 14:58:18 -0400

    The program main flow ><git add .

---
+++ Network.py
@@ -0,0 +1,16 @@
+
+class Network:
+    def __init__(self):
+        pass
+
+    def addInputLayer(self, dimension) -> None:
+        pass
+
+    def addHiddenLayer(self, neurons, activation) -> None:
+        pass
+
+    def addOutputLayer(self, dimension, activation) -> None:
+        pass
+
+    def fit(self, inputs, labels, loss, batch_size=20, epoch=100) -> []:
+        pass
---
+++ main.py
@@ -0,0 +1,64 @@
+from Network import Network
+import numpy as np
+
+
+# Data generate function
+def generate_linear(n=100):
+    pts = np.random.uniform(0, 1, (n, 2))
+    inputs = []
+    labels = []
+
+    for pt in pts:
+        inputs.append([pt[0], pt[1]])
+        distance = (pt[0] - pt[1]) / 1.414
+
+        if pt[0] > pt[1]:
+            labels.append(0)
+        else:
+            labels.append(1)
+
+    return np.array(inputs), np.array(labels).reshape(n, 1)
+
+
+def generate_XOR_easy():
+    inputs = []
+    labels = []
+
+    for i in range(11):
+        inputs.append([0.1 * i, 0.1 * i])
+        labels.append(0)
+
+        if 0.1 * i == 0.5:
+            continue
+
+        inputs.append([0.1 * i, 1 - 0.1 * i])
+        labels.append(1)
+
+    return np.array(inputs), np.array(labels).reshape(21, 1)
+
+
+# Choose the training data
+print("Chose the data set:")
+print("1. linear")
+print("2. XOR")
+choice = int(input())
+
+# Build training data
+x = np.array([], np.float)
+labels = np.array([], np.int)
+if choice == 1:
+    x, labels = generate_linear()
+elif choice == 2:
+    x, labels = generate_XOR_easy()
+
+# Build the neuron network
+model = Network()
+model.addInputLayer(dimension=2)
+model.addHiddenLayer(neurons=2, activation="ReLU")
+model.addHiddenLayer(neurons=2, activation="ReLU")
+model.addOutputLayer(dimension=1, activation="sigmoid")
+
+# Training
+lossValue, hitRate = model.fit(inputs=x, labels=labels, loss="cross_entropy")
+
+# Show graph below
